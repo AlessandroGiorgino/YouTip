f41b5e3f0fdb8c7a341b93c402894dbd6902d469
AlessandroGiorgino <alessandrogiorgino95@gmail.com>
2023-08-28 10:24:26 +0200

    c'è un problemino, non fa il serve perché no trova il tema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,13 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './header/navbar/navbar.component';
 import { HomeComponent } from './home/home/home.component';
 import { StartComponent } from './tipsPage/start/start.component';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    StartComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, StartComponent],
+  imports: [BrowserModule, AppRoutingModule, MenubarModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
