6e01f640964134d6b5c7e313451c4002e511f3de
AlessandroGiorgino <alessandrogiorgino95@gmail.com>
2023-08-28 15:48:51 +0200

    envi

---
+++ src/environments/writeEnv.ts
@@ -0,0 +1,27 @@
+const setEnv = () => {
+  // Configure Angular `environment.ts` file path
+  const targetPath = './src/environments/environment.ts';
+  // Load node modules
+  require('dotenv').config({
+    path: '.env',
+  });
+  // `environment.ts` file structure
+  const envConfigFile = `export const environment = {
+  API_KEY: '${process.env['API_KEY']}',
+  production: true,
+};
+`;
+
+  require('fs').writeFile(targetPath, envConfigFile, (err: any) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    } else {
+      console.log(
+        `Angular environment.ts file generated correctly at ${targetPath} \n`
+      );
+    }
+  });
+};
+
+setEnv();
