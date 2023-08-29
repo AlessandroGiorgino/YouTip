f6103e454a7c105eb381aaf971111668498a3c60
AlessandroGiorgino <alessandrogiorgino95@gmail.com>
2023-08-29 15:09:23 +0200

    prime modifiche alla navbar con routerlink e colorini per pagine

--- src/app/header/navbar/navbar.component.ts
+++ src/app/header/navbar/navbar.component.ts
@@ -13,91 +13,94 @@ export class NavbarComponent implements OnInit {
   ngOnInit() {
     this.items = [
       {
-        label: 'File',
-        icon: 'pi pi-fw pi-file',
-        items: [
-          {
-            label: 'New',
-            icon: 'pi pi-fw pi-plus',
-            items: [
-              {
-                label: 'Bookmark',
-                icon: 'pi pi-fw pi-bookmark',
-              },
-              {
-                label: 'Video',
-                icon: 'pi pi-fw pi-video',
-              },
-            ],
-          },
-          {
-            label: 'Delete',
-            icon: 'pi pi-fw pi-trash',
-          },
-          {
-            separator: true,
-          },
-          {
-            label: 'Export',
-            icon: 'pi pi-fw pi-external-link',
-          },
-        ],
-      },
-      {
-        label: 'Edit',
+        label: 'Start',
         icon: 'pi pi-fw pi-pencil',
-        items: [
-          {
-            label: 'Left',
-            icon: 'pi pi-fw pi-align-left',
-          },
-          {
-            label: 'Right',
-            icon: 'pi pi-fw pi-align-right',
-          },
-          {
-            label: 'Center',
-            icon: 'pi pi-fw pi-align-center',
-          },
-          {
-            label: 'Justify',
-            icon: 'pi pi-fw pi-align-justify',
-          },
-        ],
+        routerLink: ['start'],
+        // items: [
+        //   {
+        //     label: 'New',
+        //     icon: 'pi pi-fw pi-plus',
+        //     items: [
+        //       {
+        //         label: 'Bookmark',
+        //         icon: 'pi pi-fw pi-bookmark',
+        //       },
+        //       {
+        //         label: 'Video',
+        //         icon: 'pi pi-fw pi-video',
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     label: 'Delete',
+        //     icon: 'pi pi-fw pi-trash',
+        //   },
+        //   {
+        //     separator: true,
+        //   },
+        //   {
+        //     label: 'Export',
+        //     icon: 'pi pi-fw pi-external-link',
+        //   },
+        // ],
       },
       {
-        label: 'Users',
-        icon: 'pi pi-fw pi-user',
-        items: [
-          {
-            label: 'New',
-            icon: 'pi pi-fw pi-user-plus',
-          },
-          {
-            label: 'Delete',
-            icon: 'pi pi-fw pi-user-minus',
-          },
-          {
-            label: 'Search',
-            icon: 'pi pi-fw pi-users',
-            items: [
-              {
-                label: 'Filter',
-                icon: 'pi pi-fw pi-filter',
-                items: [
-                  {
-                    label: 'Print',
-                    icon: 'pi pi-fw pi-print',
-                  },
-                ],
-              },
-              {
-                icon: 'pi pi-fw pi-bars',
-                label: 'List',
-              },
-            ],
-          },
-        ],
+        label: 'Home',
+        icon: 'pi pi-fw pi-file',
+        routerLink: [''],
+
+        //   items: [
+        //     {
+        //       label: 'Left',
+        //       icon: 'pi pi-fw pi-align-left',
+        //     },
+        //     {
+        //       label: 'Right',
+        //       icon: 'pi pi-fw pi-align-right',
+        //     },
+        //     {
+        //       label: 'Center',
+        //       icon: 'pi pi-fw pi-align-center',
+        //     },
+        //     {
+        //       label: 'Justify',
+        //       icon: 'pi pi-fw pi-align-justify',
+        //     },
+        //   ],
+        // },
+        // {
+        //   label: 'Users',
+        //   icon: 'pi pi-fw pi-user',
+        //   items: [
+        //     {
+        //       label: 'New',
+        //       icon: 'pi pi-fw pi-user-plus',
+        //     },
+        //     {
+        //       label: 'Delete',
+        //       icon: 'pi pi-fw pi-user-minus',
+        //     },
+        //     {
+        //       label: 'Search',
+        //       icon: 'pi pi-fw pi-users',
+        //       items: [
+        //         {
+        //           label: 'Filter',
+        //           icon: 'pi pi-fw pi-filter',
+        //           items: [
+        //             {
+        //               label: 'Print',
+        //               icon: 'pi pi-fw pi-print',
+        //             },
+        //           ],
+        //         },
+        //         {
+        //           icon: 'pi pi-fw pi-bars',
+        //           label: 'List',
+        //         },
+        //       ],
+        //     },
+        //   ],
       },
       {
         label: 'Events',
