deec1db776a5dcab149c1f1c82acea77f14d5339
AlessandroGiorgino <alessandrogiorgino95@gmail.com>
2023-08-29 13:18:01 +0200

    aggiustata possibilità scss su component primeng

--- src/app/header/navbar/navbar.component.ts
+++ src/app/header/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class NavbarComponent implements OnInit {
   items: MenuItem[] | undefined;
