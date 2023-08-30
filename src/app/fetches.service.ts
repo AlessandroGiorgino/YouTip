0ef9a6cb570d97241a05a173d8ed92fd8be69c5a
AlessandroGiorgino <alessandrogiorgino95@gmail.com>
2023-08-30 14:59:30 +0200

    fixed problem with env

--- src/app/fetches.service.ts
+++ src/app/fetches.service.ts
@@ -7,5 +7,6 @@ import { environment } from 'src/environments/environment.development';
 export class FetchesService {
   pssApiFootPred: string = environment.API_KEY_FOOT_PRED;
   pssApiFootNew: string = environment.API_KEY_AP_FOOT;
+  pssForFirebase:string=environment.
   constructor() {}
 }
