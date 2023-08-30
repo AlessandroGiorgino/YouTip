2716e42690cdf736aff86ccb6ab9b3bdb29edeb6
AlessandroGiorgino <alessandrogiorgino95@gmail.com>
2023-08-30 17:40:17 +0200

    primo tentativo di get

--- src/app/fetches.service.ts
+++ src/app/fetches.service.ts
@@ -1,12 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
-
 @Injectable({
   providedIn: 'root',
 })
 export class FetchesService {
+  //footpred
   pssApiFootPred: string = environment.API_KEY_FOOT_PRED;
+
+  //apifoot
   pssApiFootNew: string = environment.API_KEY_AP_FOOT;
-  pssForFirebase:string=environment.
-  constructor() {}
+  urlApiFootballFixtures: string =
+    'https://api-football-v1.p.rapidapi.com/v3/fixtures';
+  //firebase
+  pssApiFirebase: string = environment.API_KEY_FIREBASE;
+
+  //aray partite serie a settimana
+  serieAweekFixtures: [] = [];
+  constructor(private http: HttpClient) {}
+  //metodo get su api foot nuova per serie a
+  optionsFixtureSerieA = {
+    params: {
+      league: '135',
+      season: '2023',
+      from: '2023-09-01',
+      to: '2023-09-03',
+    },
+    headers: {
+      'X-RapidAPI-Key': this.pssApiFootNew,
+      'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com',
+    },
+  };
+  getFixturesSerieA() {
+    return this.http.get(
+      this.urlApiFootballFixtures,
+      this.optionsFixtureSerieA
+    );
+  }
 }
--- src/app/tipsPage/start/start.component.ts
+++ src/app/tipsPage/start/start.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { FetchesService } from 'src/app/fetches.service';
 
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.scss'],
 })
-export class StartComponent {}
+export class StartComponent {
+  constructor(private srv: FetchesService) {}
+  ngOnInit(): void {
+    this.srv.getFixturesSerieA().subscribe((res) => {
+      console.log(res);
+    });
+  }
+}
