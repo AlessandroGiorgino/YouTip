ac0d7aa3d1620074bc4e6f11c7de49bf064fae53
AlessandroGiorgino <alessandrogiorgino95@gmail.com>
2023-08-30 14:54:25 +0200

    ..

--- src/app/fetches.service.ts
+++ src/app/fetches.service.ts
@@ -6,5 +6,6 @@ import { environment } from 'src/environments/environment.development';
 })
 export class FetchesService {
   pssApiFootPred: string = environment.API_KEY_FOOT_PRED;
+  pssApiFootNew: string = environment.API_KEY_AP_FOOT;
   constructor() {}
 }
