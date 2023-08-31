42ac16aabb119241682d5fab686f9e99131c1035
AlessandroGiorgino <alessandrogiorgino95@gmail.com>
2023-08-31 17:20:02 +0200

    creation of  serie B's modal

--- src/app/modals/match-list/match-list.component.ts
+++ src/app/modals/match-list/match-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { StartComponent } from 'src/app/tipsPage/start/start.component';
 
 @Component({
   selector: 'app-match-list',
   templateUrl: './match-list.component.html',
-  styleUrls: ['./match-list.component.scss']
+  styleUrls: ['./match-list.component.scss'],
 })
 export class MatchListComponent {
-
+  constructor(private start: StartComponent) {}
 }
